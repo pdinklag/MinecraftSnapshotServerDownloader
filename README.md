fd1c4eebddd28042c3d39a530e8b5744b5b568a3
pdinklag <pdinklag@gmail.com>
2018-06-17 10:18:13 +0200

    Use SHA1 hash to verify file, don't download if already got latest, add silent mode

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ By default, it will download the latest snapshot into the current working direct
 You can change the output filename via the command-line parameters (see below or use `--help`).
 
 ```
-usage: mcdlsnapshot.py [-h] [-o OUTPUT] [-d DIR] [-p PREFIX]
+usage: mcdlsnapshot.py [-h] [-o OUTPUT] [-d DIR] [-p PREFIX] [-s]
 
 Downloads the latest Minecraft snapshot server.
 
@@ -23,4 +23,15 @@ optional arguments:
   -d DIR, --dir DIR     the output directory
   -p PREFIX, --prefix PREFIX
                         the server filename prefix
+  -s, --silent          silent mode - print filename only, and only if new
+                        version was downloaded
 ```
+
+### Return Code
+The script will return _success_ (error code 0) if a new version was downloaded successfully or if the output file already is the latest version (verified via its SHA1 hash if it already exists).
+
+On any kind of failure, error code 1 is returned.
+
+### Silent Mode
+For automated downloading in host scripts, _silent mode_ can be helpful. In silent mode, nothing is printed to the output except the output file name - and only if it was actually downloaded (i.e. if an update to the latest version has been performed).
+
--- mcdlsnapshot.py
+++ mcdlsnapshot.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 import argparse
+import hashlib
 import json
 import urllib.request
 import os
 import sys
+
+def sha1(filename):
+    with open(filename, 'rb') as file:
+        return hashlib.sha1(file.read()).hexdigest()
 
 parser = argparse.ArgumentParser(
     description='Downloads the latest Minecraft snapshot server.')
@@ -16,25 +21,27 @@ parser.add_argument('-d', '--dir',
 parser.add_argument('-p', '--prefix',
                     help='the server filename prefix',
                     default='minecraft_server.')
+parser.add_argument('-s', '--silent',
+                    action='store_true',
+                    help='silent mode - print filename only, and only if new version was downloaded')
 
 args = parser.parse_args()
 
-print('Retrieving manifest ...')
+if not args.silent: print('Retrieving manifest ...')
 manifest_url = 'https://launchermeta.mojang.com/mc/game/version_manifest.json'
 manifest = json.loads(
     str(urllib.request.urlopen(manifest_url).read(), 'utf-8'))
 
 latest = manifest['latest']['snapshot']
-print('Latest snapshot version is ' + latest)
+if not args.silent: print('Latest snapshot version is ' + latest)
 for version in manifest['versions']:
     if version['id'] == latest:
-        print('Retrieving meta information ...')
+        if not args.silent: print('Retrieving meta information ...')
         meta = json.loads(
             str(urllib.request.urlopen(version['url']).read(), 'utf-8'))
 
         server_url = meta['downloads']['server']['url']
-        server_size = int(meta['downloads']['server']['size'])
-        print('Downloading: ' + server_url)
+        server_hash = meta['downloads']['server']['sha1']
 
         if args.output:
             server_filename = args.output
@@ -42,16 +49,22 @@ for version in manifest['versions']:
             server_filename = os.path.join(
                 args.dir, args.prefix + latest + '.jar')
 
+        if os.path.isfile(server_filename):
+            if sha1(server_filename) == server_hash:
+                if not args.silent: print('Nothing to do!')
+                sys.exit(0)
+
+        if not args.silent: print('Downloading: ' + server_url)
         urllib.request.urlretrieve(
             server_url,
             server_filename)
 
-        if os.path.getsize(server_filename) == server_size:
-            print('Success: ' + server_filename)
+        if sha1(server_filename) == server_hash:
+            print(server_filename)
             sys.exit(0)
         else:
-            print('Error: size of downloaded file does not match!')
+            if not args.silent: print('Error: size of downloaded file does not match!')
             sys.exit(1)
 
-print('No meta information found for ' + latest)
+if not args.silent: print('No meta information found for ' + latest)
 sys.exit(1)
